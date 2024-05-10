7f513649ba2729591ffbb920fd4f60550111b8fd
Siddhantg2002 <siddhantg2002@gmail.com>
2024-05-10 21:34:24 +0530

    added

--- Front End/HTML CSS JS/Arcadia/public/Guess the Number/normal.js
+++ Front End/HTML CSS JS/Arcadia/public/Guess the Number/normal.js
@@ -12,7 +12,7 @@ function normal() {
           // Check if input is a valid number
           playGame(userInput, targetNumber);
         } else {
-          openModal();
+         alert("Please Enter valid number")
         }
       }
     });
@@ -54,21 +54,3 @@ function normal() {
     }
   }
 }
-var modal = document.getElementById("myModal");
-
-// Function to open the modal
-function openModal() {
-  modal.style.display = "block";
-}
-
-// Function to close the modal
-function closeModal() {
-  modal.style.display = "none";
-}
-
-// Close the modal if the user clicks outside of it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
--- Front End/REACT/Weather-App/src/Components/useWeatherData.jsx
+++ Front End/REACT/Weather-App/src/Components/useWeatherData.jsx
@@ -3,7 +3,9 @@ import { useState, useEffect } from "react";
 const useWeatherData = (city ) => {
   const [weatherData, setWeatherData] = useState(null);
   const API_URL = "https://api.openweathermap.org/data/2.5/weather?units=metric";
-  const API_KEY = "9d45261bda5cb03e32a634d21dfa1a14";
+  const API_KEY = import.meta.env.VITE_OPENWEATHERMAP_API_KEY;
+
+  // Fetch weather data from API
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -13,7 +15,6 @@ const useWeatherData = (city ) => {
         }
         const data = await res.json();
         setWeatherData(data);
-        console.log(data);
       } catch (error) {
         console.log(error.message);
         alert("Failed to fetch weather data. Please try again later.");
@@ -22,6 +23,7 @@ const useWeatherData = (city ) => {
 
     if (city !== "") {
       fetchData();
+
     }
   }, [city]);
 
